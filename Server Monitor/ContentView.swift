4a82f87d088acd6ba7f9d38b5148d889930c8973
JE <james.erger@rocketmail.com>
2026-01-31 12:18:52 -0700

    Initial Commit

---
+++ Server Monitor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Server Monitor
+//
+//  Created by Gandalf on 1/31/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Server Monitor/Server_MonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Server_MonitorApp.swift
+//  Server Monitor
+//
+//  Created by Gandalf on 1/31/26.
+//
+
+import SwiftUI
+
+@main
+struct Server_MonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
